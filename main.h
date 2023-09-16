bc98ff724a3771a6ddc08ded62f2be5825a13918
slimouch <saloua.sarkadi@gmail.com>
2023-09-16 10:32:46 -0700

    update 1

---
+++ main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+void *myRealloc(void *p, size_t old, size_t n);
+char **separatrices(char *s);
+void readCommand(const char *programName);
+void forkAndExecute(char **argc, const char *programName);
+size_t myStrlen(char *str);
+char *mystrCpy(char *dest, char *src);
+int mystrCmp(char *str1, char *str2);
+char *mystrCat(char *dest, char *src);
+void handleExit(char **argc);
+void freeTok(char **tokens);
+extern char **environ;
+char *Which(char *cmd);
+void ChildExecute(char **argc, const char *programName);
+char *Getenvi(char *name);
+char *Duplicate(const char *s);
+#endif
